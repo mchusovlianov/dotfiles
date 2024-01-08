93a32a3627df2f1a316b120a0c44eb6ed4728c68
mchusovlyanov <mchusovlyanov@vio.com>
2024-01-08 15:43:40 +0100

    add install.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+git clone --depth 1 https://github.com/unixorn/fzf-zsh-plugin.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-zsh-plugin
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
